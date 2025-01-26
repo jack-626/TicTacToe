cf63be2a491e15cfe9f124f75fdb77174708aefd
Jack Bennett <155745187+jack-626@users.noreply.github.com>
2025-01-26 00:01:51 +0000

    Added comments. Added pause to the console when game is finished.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 /*
-  _______   _____    _____     _______               _____     _______    ____    ______
- |__   __| |_   _|  / ____|   |__   __|     /\      / ____|   |__   __|  / __ \  |  ____|
-	| |      | |   | |           | |       /  \    | |           | |    | |  | | | |__
-	| |      | |   | |           | |      / /\ \   | |           | |    | |  | | |  __|
-	| |     _| |_  | |____       | |     / ____ \  | |____       | |    | |__| | | |____
-	|_|    |_____|  \_____|      |_|    /_/    \_\  \_____|      |_|     \____/  |______|
+	  _______   _____    _____     _______               _____     _______    ____    ______
+	 |__   __| |_   _|  / ____|   |__   __|     /\      / ____|   |__   __|  / __ \  |  ____|
+		| |      | |   | |           | |       /  \    | |           | |    | |  | | | |__
+		| |      | |   | |           | |      / /\ \   | |           | |    | |  | | |  __|
+		| |     _| |_  | |____       | |     / ____ \  | |____       | |    | |__| | | |____
+		|_|    |_____|  \_____|      |_|    /_/    \_\  \_____|      |_|     \____/  |______|
 	
 	Created by Jack Bennett
 	25th January 2025
@@ -12,78 +12,96 @@
 
 #include <iostream>
 
+
+#pragma region Declare Functions
 void drawBoard();
 bool checkWin(char player);
 bool checkDraw();
 void chooseSpot(char player);
+#pragma endregion
 
+#pragma region Declare & Initialize Global Variables
+//Player Symbols
 const char player1 = 'X';
 const char player2 = 'O';
 
+//Create 2D array for the board
 char board[3][3]
 {
 	{'1', '2', '3'},
 	{'4', '5', '6'},
 	{'7', '8', '9'}
 };
+#pragma endregion
 
 int main()
 {
 	//Game Loop
 	while (true)
 	{
+		//Draw the board.
 		drawBoard();
 
 		//Player 1's turn
-		chooseSpot(player1);
-		if (checkWin(player1))
+		chooseSpot(player1); // Get Player 1's input.
+		if (checkWin(player1)) // Check if there was a win
 		{
+			//Clear the screen, draw the board again, output a winning message and break out of the game loop.
 			system("cls");
 			drawBoard();
 			std::cout << "\nPlayer " << player1 << " Wins! Congrats!" << std::endl;
 			break;
 		}
-		if (checkDraw())
+		if (checkDraw()) // Check if there was a draw
 		{
+			//Clear the screen, draw the board again, output a draw message and break out of the game loop.
 			system("cls");
 			drawBoard();
 			std::cout << "\nIt's a draw!" << std::endl;
 			break;
 		}
-		system("cls");
 
+		//End of player 1's turn. Clear the screen and redraw the board.
+		system("cls");
 		drawBoard();
 
 		//Player 2's turn
-		chooseSpot(player2);
-		if (checkWin(player2))
+		chooseSpot(player2); // Get Player 2's input.
+		if (checkWin(player2)) // Check if there was a win
 		{
+			//Clear the screen, draw the board again, output a winning message and break out of the game loop.
 			system("cls");
 			drawBoard();
 			std::cout << "\nPlayer " << player2 << " Wins! Congrats!" << std::endl;
 			break;
 		}
-		if (checkDraw())
+		if (checkDraw()) // Check if there was a draw
 		{
+			//Clear the screen, draw the board again, output a draw message and break out of the game loop.
 			system("cls");
 			drawBoard();
 			std::cout << "\nIt's a draw!" << std::endl;
 			break;
 		}
+
+		//End of player 2's turn. Clear the screen.
 		system("cls");
 	}
 
+	//Output an end of game message.
 	std::cout << "\n-------------------\n";
 	std::cout << "Thanks for playing!";
 	std::cout << "\n-------------------\n\n\n";
+	system("pause"); //Suspend processing so the player can read the game results.
 	return 0;
 }
 
 void drawBoard()
 {
 	//Welcome Message
-
 	std::cout << "-----------------------" << "\nWelcome to Tic-Tac-Toe! " << "\n-----------------------" << std::endl;
+
+	//Loop through the board 2D array and print it to the screen, with some spacers.
 	std::cout << " ------------- ";
 	for (int i = 0; i < 3; i++)
 	{
@@ -158,20 +176,20 @@ bool checkDraw()
 void chooseSpot(char player)
 {
 	int choice;
-	while (true)
+	while (true) //Input validation loop
 	{
-		std::cout << "\nPlayer " << player << ", choose a spot. (1-9)" << std::endl;
-		std::cin >> choice;
+		std::cout << "\nPlayer " << player << ", choose a spot. (1-9)" << std::endl; //Output a prompt for the player to input a number.
+		std::cin >> choice; //Get the players input.
 		if (!std::cin.fail()) // Check if input is an integer
 		{
-			if (choice >= 1 && choice <= 9)
+			if (choice >= 1 && choice <= 9) //Check if the input is within the desired range.
 			{
-				//Convert choice to an index for the board array.
+				//Convert input to an index for the board array.
 				int row = (choice - 1) / 3;
 				int column = (choice - 1) % 3;
-				if (board[row][column] != 'O' && board[row][column] != 'X')
+				if (board[row][column] != 'O' && board[row][column] != 'X') //Check if the chosen spot is already taken.
 				{
-					board[row][column] = player;
+					board[row][column] = player; //Set the spot on the board to the players symbol and break out of the input validation loop.
 					break;
 				}
 				else
@@ -188,7 +206,7 @@ void chooseSpot(char player)
 		{
 			std::cout << "Input a valid number." << std::endl;
 		}
-		//Validations failed, clear input stream and ask for input again.
+		//Validations failed, clear input stream.
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
